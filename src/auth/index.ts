b4e75d60c790a9e95ced8c51f371b788f7d88418
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2024-04-28 23:17:13 -0300

    feat: setting the project

---
+++ src/auth/index.ts
@@ -0,0 +1,36 @@
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import NextAuth from 'next-auth'
+import GitHub from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+import Nodemailer from 'next-auth/providers/nodemailer'
+
+import { prisma } from '../lib/prisma'
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  pages: {
+    signIn: '/login',
+    signOut: '/login',
+    error: '/login',
+    verifyRequest: '/login',
+    newUser: '/home',
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Nodemailer({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+    GitHub,
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+  ],
+})
